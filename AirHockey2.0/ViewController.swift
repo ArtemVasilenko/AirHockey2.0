5da39db9d2a5e40e1444c573e2e23b850375845f
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-15 11:10:14 +0300

    Initial Commit

---
+++ AirHockey2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirHockey2.0
+//
+//  Created by Артем on 5/15/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
